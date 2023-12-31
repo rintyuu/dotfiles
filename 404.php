868b32e0bd36f509cf7b16adec74ee6a476e2f38
rintyuu <62674887+theusermc@users.noreply.github.com>
2023-12-31 01:57:33 -0800

    Upload 404.php
    really basic apache2 404 page written in php to go along with peanut and butter sandwich example.. lol

---
+++ 404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="why are you sharing this" />
+    <title>404 not found</title>
+    <link
+      rel="shortcut icon"
+      href="https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2F2ff3a645-91cc-42fd-bae2-3ba8fb0148a0_icon.png?v=1623273460343"
+    />
+
+    <link rel="stylesheet" href="https://rintyuu.glitch.me/assets/style.css" />
+
+    <script src="https://rintyuu.glitch.me/assets/script.js" defer></script>
+    <meta name="theme-color" content="#000000" />
+</head>
+<body style="background-color: #000">
+    <h1>
+        404 not found
+    </h1>
+    <br />
+    <p>
+        what have you done?
+    </p>
+    <p>i am also 99.999% sure that this is a client-side error..</p>
+    <br />
+    <br />
+    
+    <?php
+    // Get server details
+    $serverName = $_SERVER['SERVER_NAME'];
+    $serverSoftware = $_SERVER['SERVER_SOFTWARE'];
+
+    // Display server details
+    echo "<i>$serverSoftware Server at $serverName Port 80</i>";
+    ?>
+</body>
+</html>
